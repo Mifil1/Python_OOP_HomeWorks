7f9a9aa06813a7873048ca5400213e14bde3372e
Mifil_ <mifil@gmail.com>
2023-04-11 00:21:43 +0300

    First commit in task 1 "Phone"

--- Phone.py
+++ Phone.py
@@ -32,19 +32,20 @@ class MobilePhone:
         return self._call_counter
 
     # Magic functions
-    def __init__(self, pnumber = ""):
+    def __init__(self, pnumber=""):
         if self.set_phone_number(pnumber):
             pass
         else:
             self.__p_number = MobilePhone._create_number()
-            #self.__p_number = self.__create_number()
+            # self.__p_number = self.__create_number()
 
     # static functions
     @staticmethod
     def _create_number() -> str:
-        "Not work"
+        """Not work"""
         MobilePhone.__count_numbers += 1
-        return "0" * (11 - len(str(MobilePhone.__count_numbers))) + str(MobilePhone.__count_numbers)
+        return "0" * (11 - len(str(MobilePhone.__count_numbers))) + \
+            str(MobilePhone.__count_numbers)
 
     # other functions
 
@@ -52,7 +53,8 @@ class MobilePhone:
         self._call_counter += 1
 
     def info(self):
-        return f"Phone number is {self.__p_number}\nCount calls = {self._call_counter}"
+        return f"Phone number is {self.__p_number}\nCount calls = " \
+               f"{self._call_counter}"
 
 
 ###########################
@@ -63,16 +65,18 @@ class MobilePhone:
 ###########################
 # Блок реалізації функцій #
 ###########################
-def caller(phone: MobilePhone, count=10):
+def caller(phone: MobilePhone, count=10) -> None:
     for i in range(count):
         phone.call()
 
-def count_calls(lst:list) -> int:
+
+def count_calls(lst: list) -> int:
     temp_sum = 0
     for i in lst:
         if isinstance(i, MobilePhone):
             temp_sum += i.get_count_calls()
     return temp_sum
+
 
 ###########################
 #       Кінець блоку      #
@@ -87,24 +91,23 @@ while True:
     print("Введи номер завдання. От 1 до 3. Введи 0 чтобы выйти. ")
     your_choice = int(input())
     print(f"Вы ввели: {str(your_choice)}")
-    if(your_choice):
+    if  your_choice:
         print(f"Завдання №{your_choice}")
-    match (your_choice):
+    match your_choice:
         case 1:
             print("")
-            #Phone 1
+            # Phone 1
             phone1 = MobilePhone()
             phone1.set_phone_number("88005553535")
             caller(phone1, 10)
             print(phone1.info())
-            #Phone 2
+            # Phone 2
             phone2 = MobilePhone("78732187928")
             phone2.set_phone_number("00006660000")
             caller(phone2, 2)
             print(phone2.info())
-            #Phone 3
+            # Phone 3
             phone3 = MobilePhone()
-            # phone3.set_phone_number("1")
             caller(phone3, 18)
             print(phone3.info())
 
